bfc18b53b116f1095cbfd2cfc7735f4f4e79e433
akshat262005 <162410084+akshat262005@users.noreply.github.com>
2025-04-16 18:10:12 +0530

    Add files via upload

---
+++ NetBeansProjects/Airline Management System/src/airline/management/system/Cancel.java
@@ -0,0 +1,148 @@
+package airline.management.system;
+
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class Cancel extends JFrame implements ActionListener {
+
+    JTextField tfpnr;
+    JLabel tfname, cancellationno, lblfcode, lbldateoftravel;
+    JButton fetchButton, flight;
+
+
+    public Cancel() {
+
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+
+        Random random = new Random();
+
+        JLabel heading = new JLabel("CANCELLATION");
+        heading.setBounds(180, 20, 250, 35);
+        heading.setFont(new Font("Tahoma", Font.PLAIN, 32));
+        add(heading);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("airline/management/system/icons/cancel.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(250, 250, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(470, 120, 250, 250);
+        add(image);
+
+        JLabel lblpnr = new JLabel("PNR Number");
+        lblpnr.setBounds(60, 80, 150, 25);
+        lblpnr.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lblpnr);
+
+        tfpnr = new JTextField();
+        tfpnr.setBounds(220, 80, 150, 25);
+        add(tfpnr);
+
+        fetchButton = new JButton("Show Details");
+        fetchButton.setBackground(Color.BLACK);
+        fetchButton.setForeground(Color.WHITE);
+        fetchButton.setBounds(380, 80, 120, 25);
+        fetchButton.addActionListener(this);
+        add(fetchButton);
+
+        JLabel lblname = new JLabel("Name");
+        lblname.setBounds(60, 130, 150, 25);
+        lblname.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lblname);
+
+        tfname = new JLabel();
+        tfname.setBounds(220, 130, 150, 25);
+        add(tfname);
+
+        JLabel lblcancellation = new JLabel("Cancellation No.");
+        lblcancellation.setBounds(60, 180, 150, 25);
+        lblcancellation.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lblcancellation);
+
+        cancellationno = new JLabel("" + random.nextInt(1000000));
+        cancellationno.setBounds(220, 180, 150, 25);
+        add(cancellationno);
+
+        JLabel lblfc = new JLabel("Flight Code");
+        lblfc.setBounds(60, 230, 150, 25);
+        lblfc.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lblfc);
+
+        lblfcode = new JLabel();
+        lblfcode.setBounds(220, 230, 150, 25);
+        add(lblfcode);
+
+        JLabel lbldot = new JLabel("Date");
+        lbldot.setBounds(60, 280, 150, 25);
+        lbldot.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lbldot);
+
+        lbldateoftravel = new JLabel();
+        lbldateoftravel.setBounds(220, 280, 150, 25);
+        add(lbldateoftravel);
+
+        flight = new JButton("Cancel");
+        flight.setBackground(Color.black);
+        flight.setForeground(Color.WHITE);
+        flight.setBounds(220, 330, 120, 25);
+        flight.addActionListener(this);
+        add(flight);
+
+        setSize(800, 450);
+        setLocation(350, 150);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == fetchButton) {
+            String pnr = tfpnr.getText();
+            try {
+                Conn conn = new Conn();
+
+                String query = "select * from reservation where PNR = '" + pnr + "'";
+
+                ResultSet rs = conn.s.executeQuery(query);
+
+                if (rs.next()) {
+                    tfname.setText(rs.getString("name"));
+                    lblfcode.setText(rs.getString("flightcode"));
+                    lbldateoftravel.setText(rs.getString("ddate"));
+
+                } else {
+                    JOptionPane.showMessageDialog(null, "Please Enter Correct PNR");
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (ae.getSource() == flight) {
+            String name = tfname.getText();
+            String pnr = tfpnr.getText();
+            String cancelno = cancellationno.getText();
+            String fcode = lblfcode.getText();
+            String date = lbldateoftravel.getText();
+
+            try {
+                Conn conn = new Conn();
+
+                String query = "insert into cancel values('" + pnr + "', '" + name + "', '" + cancelno + "', '" + fcode + "', '" + date + "')";
+
+                conn.s.executeUpdate(query);
+                conn.s.executeUpdate("delete from reservation where PNR = '"+pnr+"'");
+                
+                JOptionPane.showMessageDialog(null, "Ticket Cancelled");
+                setVisible(false);
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } 
+    }
+
+    public static void main(String[] args) {
+        new Cancel();
+    }
+}
---
+++ NetBeansProjects/Airline Management System/src/airline/management/system/Conn.java
@@ -0,0 +1,17 @@
+package airline.management.system;
+
+import java.sql.*;
+public class Conn {
+    Connection c;
+    Statement s;
+    
+    public Conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c=DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem", "root", "zxcvbnm");
+            s=c.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
